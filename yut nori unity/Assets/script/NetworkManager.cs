36d21a3f47e9948b2bd3f2cea29e97326470fef0
이진수 <leejinsoo0412@gmail.com>
2023-03-16 16:37:49 +0900

    unity

---
+++ yut nori unity/Assets/script/NetworkManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //public void CreateRoom() => PhotonNetwork.CreateRoom();
+}
